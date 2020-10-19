1e1900e7c240a3aabb8944480511aef0ec4ce7b4
louis2516 <louis.bonnenfant13@gmail.com>
2020-10-19 12:09:32 +0200

    nouveau dossier

---
+++ exercice1.php
@@ -0,0 +1,16 @@
+<?php
+$ville=array('Poitiers' =>'86' ,'Paris'=>'75','La rochelle'=>'17');
+echo $ville['Poitiers'];
+if (array_key_exists('nice',$ville)){
+  echo "<br />Nice exist";
+}
+else {
+  echo "<br /> Nice existe pas";
+}
+
+
+
+
+
+
+ ?>
